3cd8579d1b5001c64c61ecb1c7ec7360b4c3a8c1
AqUpd <aquupd@ya.ru>
2022-01-02 18:18:37 +0300

    no spawn in water

--- src/main/java/com/aqupd/grizzlybear/Main.java
+++ src/main/java/com/aqupd/grizzlybear/Main.java
@@ -9,14 +9,18 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.Heightmap;
 
 import java.util.Arrays;
 
@@ -72,6 +76,7 @@ public class Main implements ModInitializer {
 				GRIZZLYBEAR,
 				weight, mingroup, maxgroup // weight/min group size/max group size
 		);
+		SpawnRestrictionAccessor.callRegister(GRIZZLYBEAR, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MobEntity::canMobSpawn);
 		logInfo("Grizzly Bears mod is loaded!");
 	}
 }
